e54cd6838542b32e0e7bc6aa492f94ed6bb9c2b3
aaa <aaaaaa>
2026-06-02 09:25:03 -0300

    feat(p2): tabela para importação dos dados

---
+++ script.sql
@@ -0,0 +1,21 @@
+-- 1 Base de dados e criação de tabela - A base a ser utilizada pode ser obtida a partir do link a seguir. Há também uma versão
+-- disponível no ambiente da disciplina. Utilize a que preferir.
+-- https://www.kaggle.com/datasets/csafrit2/higher-education-students-performance-eva
+-- luation
+-- Ela deve ser importada para uma base de dados gerenciada pelo PostgreSQL. Os dados
+-- devem ser armazenados em uma tabela apropriada para as análises desejadas. Você deve
+-- identificar as colunas necessárias, de acordo com a descrição de cada item da prova.
+-- Além, é claro, de uma chave primária (de auto incremento).
+
+CREATE TABLE student_prediction ( 
+	
+    id_register SERIAL PRIMARY KEY,    
+    student_id VARCHAR(30),
+    mother_edu INTEGER,
+    father_edu INTEGER,
+    study_hrs INTEGER,
+    salary INTEGER,
+    prep_exam INTEGER,
+    grade INTEGER
+	
+);
